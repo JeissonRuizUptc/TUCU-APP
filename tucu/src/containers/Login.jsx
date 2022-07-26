cf4e8686363724c8c418ad4f16434cf7cffbf5a7
JeissonRuizUptc <90711266+JeissonRuizUptc@users.noreply.github.com>
2022-07-26 14:32:17 -0500

    [INITAL COMPONENT] adding Login Component

---
+++ tucu/src/containers/Login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './styles/Login.scss';
+
+const Login = () =>{
+    return(
+        <div className="login row">
+            <div className="column">
+                <p>este es un texto</p>
+            </div>
+            <div className="column">
+                <p>este es otro texto</p>
+            </div>
+        </div>
+    )
+}
+
+
+
+export default Login;
