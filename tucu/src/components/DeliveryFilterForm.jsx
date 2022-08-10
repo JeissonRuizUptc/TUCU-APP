3f0dff9196dd86831a582ce585b67eeda6afc55c
JeisonRuiz0 <Jeisson.Ruiz01@uptc.edu.co>
2022-08-10 07:54:34 -0500

    https://jhonatanbaron.atlassian.net/browse/SFTW1-40

--- tucu/src/components/DeliveryFilterForm.jsx
+++ tucu/src/components/DeliveryFilterForm.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './styles/DeliveryFilterForm.scss';
 
 const DeliveryFilterForm = ({tittleForm, holderText}) => {
     return (
         <div>
-            <div className="form-group">
+            <div className="form-filter form-group ">
                 <label for="exampleInputEmail1">{tittleForm}</label>
                 <input type="text" className="form-control" id="exampleText" aria-describedby="exampleText" placeholder={holderText} />
             </div>
